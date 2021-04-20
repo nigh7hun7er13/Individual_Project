68a8f0db5571c862b1598bebdc82e64b7179b944
root <dlacour@uccs.edu>
2021-04-20 03:44:11 +0000

    Init

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,11 +59,11 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = Post.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :description, :slug)
+      params.require(:post).permit(:title, :body, :description)
     end
 end
